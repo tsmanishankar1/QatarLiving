f76b224d1e88186deefaff705662bf0f91660206
ashwin.g <ashwin.g@kryptosinfosys.com>
2025-04-16 17:10:55 +0530

    Removed unwanted spaced

--- QLN.Blazor.Base/Program.cs
+++ QLN.Blazor.Base/Program.cs
@@ -7,8 +7,6 @@ using MudBlazor.Services;
 using QLN.Blazor.Base.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddMudServices();
