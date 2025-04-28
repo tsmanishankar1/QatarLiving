aaaf8209ab034978a92d858b227b73996c92066e
Jaswanth R <jaswanth.r@kryptosinfosys.com>
2025-04-28 14:22:02 +0530

    updated edit profile payload added a validation helper

---
+++ QLN.Blazor.Base/Helpers/ValidationHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace QLN.Blazor.Base.Helpers
+{
+    public static class ValidationHelper
+    {
+        public static bool IsValidPhoneNumber(string phoneNumber)
+        {
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+                return false;
+
+            var regex = new Regex(@"^\d{6,15}$");
+            return regex.IsMatch(phoneNumber);
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            var regex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+            return regex.IsMatch(email);
+        }
+    }
+}
