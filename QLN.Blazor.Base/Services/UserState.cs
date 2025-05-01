c3865fb81cb385c0def9e134fcd09d0e2d8bf215
Vignesh R <vignesh.ra@kryptosinfosys.com>
2025-05-01 20:23:44 +0530

    classifides ui updated and some bugs fixed

--- QLN.Blazor.Base/Services/UserState.cs
+++ QLN.Blazor.Base/Services/UserState.cs
@@ -3,6 +3,7 @@ public class UserState
     public string? Email { get; set; }
     public string? ProfileImageUrl { get; set; }
     public string? mobileNumber { get; set; }
+    public string? username { get; set; }
     public string? Token { get; set; }
 
     public bool IsLoggedIn => !string.IsNullOrEmpty(Email);
