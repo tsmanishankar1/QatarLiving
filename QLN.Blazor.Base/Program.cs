2eb3bc893065dbaf787cea88373be5270d9003df
ashwin.g <ashwin.g@kryptosinfosys.com>
2025-05-08 22:33:58 +0530

    Added CascadingParameter

--- QLN.Blazor.Base/Program.cs
+++ QLN.Blazor.Base/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Identity;
+using QLN.Web.Shared;
 using QLN.Web.Shared.Pages;
 using QLN.Web.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddHttpClient();
 builder.Services.AddAuthentication(options =>
     {
         options.DefaultScheme = IdentityConstants.ApplicationScheme;
@@ -15,6 +16,7 @@ builder.Services.AddAuthentication(options =>
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddWebSharedServices(builder.Configuration);
+builder.Services.AddScoped<CountryService>();
 
 var app = builder.Build();
 
