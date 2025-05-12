3760ba29170d452caa956355cbe9da7b3c17ed8a
Jaswanth R <jaswanth.r@kryptosinfosys.com>
2025-05-12 18:56:44 +0530

    Implemented Subscription listing ,Add subscription form

---
+++ QLN.Web.Shared/Models/SubscriptionPlan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLN.Web.Shared.Models
+{
+    public class SubscriptionPlan
+    {
+        public string Title { get; set; }
+        public string Price { get; set; }
+        public string Duration { get; set; }
+        public int Flyers { get; set; }
+    }
+
+}
