faef31c69a581c5b02ed33d406a5cc9d5f4bff41
Jaswanth R <jaswanth.r@kryptosinfosys.com>
2025-05-13 19:12:56 +0530

    updated the subscription UI,added a base payment form submit

--- QLN.Web.Shared/Models/SubscriptionPlan.cs
+++ QLN.Web.Shared/Models/SubscriptionPlan.cs
@@ -12,6 +12,18 @@ namespace QLN.Web.Shared.Models
         public string Price { get; set; }
         public string Duration { get; set; }
         public int Flyers { get; set; }
+        public override bool Equals(object obj)
+        {
+            return obj is SubscriptionPlan plan &&
+                   Duration == plan.Duration &&
+                   Price == plan.Price &&
+                   Flyers == plan.Flyers;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Flyers, Price, Duration);
+        }
     }
 
 }
