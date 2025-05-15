29af8dfbafb6c232419d1408a784c683edbafeed
Vignesh R <vignesh.ra@kryptosinfosys.com>
2025-05-15 10:00:05 +0530

    stored added

---
+++ QLN.Web.Shared/Components/Classifieds/StoreCard/StoreCard.cs
@@ -0,0 +1,12 @@
+namespace QLN.Web.Shared.Components.Classifieds.StoreCard
+{
+    public partial class StoreCard
+    {
+        public class StoreItem
+        {
+            public string Name { get; set; } = string.Empty;
+            public string LogoUrl { get; set; } = string.Empty;
+            public string ItemCount { get; set; } = string.Empty;
+        }
+    }
+}
