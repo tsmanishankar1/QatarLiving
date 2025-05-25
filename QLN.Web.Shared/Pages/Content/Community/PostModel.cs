3526df660c011a220e43b54ab22f902244ea08e3
Jaswanth R <jaswanth.r@kryptosinfosys.com>
2025-05-25 18:45:53 +0530

    Post Details UI setup

--- QLN.Web.Shared/Pages/Content/Community/PostModel.cs
+++ QLN.Web.Shared/Pages/Content/Community/PostModel.cs
@@ -8,6 +8,7 @@ namespace QLN.Web.Shared.Pages.Content.Community
 {
     public class PostModel
     {
+        public string Id { get; set; } 
         public string Category { get; set; }
         public string Title { get; set; }
         public string? ImageUrl { get; set; }
@@ -16,6 +17,20 @@ namespace QLN.Web.Shared.Pages.Content.Community
         public DateTime Time { get; set; }
         public int LikeCount { get; set; }
         public int CommentCount { get; set; }
+        public bool isCommented { get; set; }
+
+        public List<CommentModel> Comments { get; set; } = new();
+    }
+
+    public class CommentModel
+    {
+        public string Avatar { get; set; } = string.Empty;
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int LikeCount { get; set; }
+        public int UnlikeCount { get; set; }
+        public bool IsByCurrentUser { get; set; }
     }
 
 }
