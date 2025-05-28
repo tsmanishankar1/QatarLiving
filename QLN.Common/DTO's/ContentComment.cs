db4bd58fcf8f82ce31cfb863f19cbf13573c5d09
Grant Vine <grant.vine@crayon.com>
2025-05-28 16:47:57 +0200

    Adding Comments to the detailed respons API (slug based)

---
+++ QLN.Common/DTO's/ContentComment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace QLN.Common.Infrastructure.DTO_s
+{
+    public class ContentComment
+    {
+        [JsonPropertyName("comment_id")]
+        public string CommentId { get; set; }
+
+        [JsonPropertyName("subject")]
+        public string Subject { get; set; }
+
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("created_date")]
+        public string CreatedDate { get; set; }
+    }
+}
