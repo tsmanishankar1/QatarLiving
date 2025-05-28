ba249687279abe858a084edeb8eb3b0d1cf3ebaf
Giftson Daniel G <giftsondaniel.g@kryptosinfosys.com>
2025-05-28 18:13:33 +0530

    NewsSection and More Articles API Integ

--- QLN.Web.Shared/Pages/Content/Daily/DailyMoreArticle/DailyMoreArticleBase.cs
+++ QLN.Web.Shared/Pages/Content/Daily/DailyMoreArticle/DailyMoreArticleBase.cs
@@ -1,44 +1,7 @@
 using Microsoft.AspNetCore.Components;
+using QLN.Common.Infrastructure.DTO_s;
 public class DailyMoreArticleBase : ComponentBase
 {
-    public class Article
-{
-    public string Category { get; set; }
-    public string Title { get; set; }
-    public string ImageUrl { get; set; } 
-    public string Url { get; set; }
-}
-    public List<Article> Articles = new()
-    {
-        new Article
-        {
-            Category = "Lifestyle",
-            Title = "How to spot scam websites & social media accounts in Qatar",
-            Url = "#",
-            ImageUrl = "/images/sample_article.svg"
-        },
-        new Article
-        {
-            Category = "Finance",
-            Title = "Qatar gold prices rise by 4.86% this week",
-            Url = "#",
-            ImageUrl = "/images/sample_article.svg"
-        },
-        new Article
-        {
-            Category = "International",
-            Title = "Saudi Arabia announces Umrah season calendar",
-            Url = "#",
-            ImageUrl = "/images/sample_article.svg"
-        },
-        new Article
-        {
-            Category = "Entertainment",
-            Title = "Qatar Museums to feature new publications at 34th Doha International Book Fair",
-            Url = "#",
-            ImageUrl = "/images/sample_article.svg"
-        },
-      
-    };
-
+    [Parameter]
+    public List<ContentPost> Items { get; set; } = [];
 }
