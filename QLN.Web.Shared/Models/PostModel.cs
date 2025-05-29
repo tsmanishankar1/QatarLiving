c2fa4ac287871a42868db38539a38245a1beae6f
Jaswanth R <jaswanth.r@kryptosinfosys.com>
2025-05-29 13:33:26 +0530

    Configured and consumed the base URL from the appsetting.json

--- QLN.Web.Shared/Models/PostModel.cs
+++ QLN.Web.Shared/Models/PostModel.cs
@@ -41,6 +41,7 @@
         public string title { get; set; }
         public string slug { get; set; }
         public string description { get; set; }
+        public string image_url { get; set; }
     }
 
 
