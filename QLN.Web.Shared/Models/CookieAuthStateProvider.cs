05bd6e6cdc9d6ceee6a385101e1e50dd44c9488e
Grant Vine <grant.vine@crayon.com>
2025-05-30 18:19:26 +0200

    Additional changes

--- QLN.Web.Shared/Models/CookieAuthStateProvider.cs
+++ QLN.Web.Shared/Models/CookieAuthStateProvider.cs
@@ -1,3 +1,4 @@
+using Google.Api;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
@@ -20,7 +21,7 @@ namespace QLN.Web.Shared.Models
             _configuration = configuration;
         }
 
-        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             var httpContext = _httpContextAccessor.HttpContext;
             ClaimsPrincipal principal = new ClaimsPrincipal(new ClaimsIdentity());
@@ -30,8 +31,8 @@ namespace QLN.Web.Shared.Models
                 var tokenHandler = new JwtSecurityTokenHandler();
                 var validationParameters = new TokenValidationParameters
                 {
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
                     ValidateLifetime = false,
                     ValidateIssuerSigningKey = false,
                     SignatureValidator = (token, parameters) =>
@@ -39,69 +40,80 @@ namespace QLN.Web.Shared.Models
                         // Bypass signature validation for demo purposes
                         return new JwtSecurityToken(token);
                     },
+                    ValidIssuer = _configuration["Jwt:Issuer"],
+                    ValidAudience = _configuration["Jwt:Audience"],
                     RoleClaimType = ClaimTypes.Role,
                     NameClaimType = ClaimTypes.Name
                 };
 
                 try
                 {
-                    var jwtToken = tokenHandler.ReadJwtToken(jwt);
-                    var identity = new ClaimsIdentity("jwt");
 
-                    // Standard JWT claims
-                    if (jwtToken.Payload.TryGetValue(JwtRegisteredClaimNames.Sub, out var sub))
-                        identity.AddClaim(new Claim(JwtRegisteredClaimNames.Sub, sub.ToString()!));
-                    if (jwtToken.Payload.TryGetValue(JwtRegisteredClaimNames.Iss, out var iss))
-                        identity.AddClaim(new Claim(JwtRegisteredClaimNames.Iss, iss.ToString()!));
-                    if (jwtToken.Payload.TryGetValue(JwtRegisteredClaimNames.Aud, out var aud))
-                        identity.AddClaim(new Claim(JwtRegisteredClaimNames.Aud, aud.ToString()!));
-                    if (jwtToken.Payload.TryGetValue(JwtRegisteredClaimNames.Iat, out var iat))
-                        identity.AddClaim(new Claim(JwtRegisteredClaimNames.Iat, iat.ToString()!));
-                    if (jwtToken.Payload.TryGetValue(JwtRegisteredClaimNames.Exp, out var exp))
-                        identity.AddClaim(new Claim(JwtRegisteredClaimNames.Exp, exp.ToString()!));
-
-                    // Custom user object
-                    if (jwtToken.Payload.TryGetValue("user", out var userObj) && userObj is System.Text.Json.JsonElement userElement && userElement.ValueKind == System.Text.Json.JsonValueKind.Object)
+                    try
                     {
-                        var user = Newtonsoft.Json.Linq.JObject.Parse(userElement.GetRawText());
-                        // Map user fields to claims
-                        if (user.TryGetValue("uid", out var uid))
-                            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, uid.ToString()!));
-                        if (user.TryGetValue("name", out var name))
-                            identity.AddClaim(new Claim(ClaimTypes.Name, name.ToString()!));
-                        if (user.TryGetValue("email", out var email))
-                            identity.AddClaim(new Claim(ClaimTypes.Email, email.ToString()!));
-                        if (user.TryGetValue("is_admin", out var isAdmin))
-                            identity.AddClaim(new Claim("is_admin", isAdmin.ToString()!));
-                        if (user.TryGetValue("qlnext_user_id", out var qlUserId))
-                            identity.AddClaim(new Claim("qlnext_user_id", qlUserId.ToString()!));
-                        if (user.TryGetValue("alias", out var alias))
-                            identity.AddClaim(new Claim("alias", alias.ToString()!));
-                        if (user.TryGetValue("image", out var image))
-                            identity.AddClaim(new Claim("image", image.ToString()!));
-                        if (user.TryGetValue("status", out var status))
-                            identity.AddClaim(new Claim("status", status.ToString()!));
-                        if (user.TryGetValue("permissions", out var permissions) && permissions is Newtonsoft.Json.Linq.JArray permsArray)
-                        {
-                            foreach (var perm in permsArray)
-                                identity.AddClaim(new Claim("permission", perm.ToString()!));
-                        }
-                        if (user.TryGetValue("roles", out var roles) && roles is Newtonsoft.Json.Linq.JArray rolesArray)
-                        {
-                            foreach (var role in rolesArray)
-                                identity.AddClaim(new Claim(ClaimTypes.Role, role.ToString()!));
-                        }
-                    }
+                        var jwtToken = tokenHandler.ReadJwtToken(jwt);
+                        var identity = new ClaimsIdentity("jwt");
 
-                    principal = new ClaimsPrincipal(identity);
+                        // Standard JWT claims
+                        if (jwtToken.Payload.TryGetValue(JwtRegisteredClaimNames.Sub, out var sub))
+                            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Sub, sub.ToString()!));
+                        if (jwtToken.Payload.TryGetValue(JwtRegisteredClaimNames.Iss, out var iss))
+                            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Iss, iss.ToString()!));
+                        if (jwtToken.Payload.TryGetValue(JwtRegisteredClaimNames.Aud, out var aud))
+                            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Aud, aud.ToString()!));
+                        if (jwtToken.Payload.TryGetValue(JwtRegisteredClaimNames.Iat, out var iat))
+                            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Iat, iat.ToString()!));
+                        if (jwtToken.Payload.TryGetValue(JwtRegisteredClaimNames.Exp, out var exp))
+                            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Exp, exp.ToString()!));
+
+                        // Custom user object
+                        if (jwtToken.Payload.TryGetValue("user", out var userObj) && userObj is System.Text.Json.JsonElement userElement && userElement.ValueKind == System.Text.Json.JsonValueKind.Object)
+                        {
+                            var user = Newtonsoft.Json.Linq.JObject.Parse(userElement.GetRawText());
+                            // Map user fields to claims
+                            if (user.TryGetValue("uid", out var uid))
+                                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, uid.ToString()!));
+                            if (user.TryGetValue("name", out var name))
+                                identity.AddClaim(new Claim(ClaimTypes.Name, name.ToString()!));
+                            if (user.TryGetValue("email", out var email))
+                                identity.AddClaim(new Claim(ClaimTypes.Email, email.ToString()!));
+                            if (user.TryGetValue("is_admin", out var isAdmin))
+                                identity.AddClaim(new Claim("is_admin", isAdmin.ToString()!));
+                            if (user.TryGetValue("qlnext_user_id", out var qlUserId))
+                                identity.AddClaim(new Claim("qlnext_user_id", qlUserId.ToString()!));
+                            if (user.TryGetValue("alias", out var alias))
+                                identity.AddClaim(new Claim("alias", alias.ToString()!));
+                            if (user.TryGetValue("image", out var image))
+                                identity.AddClaim(new Claim("image", image.ToString()!));
+                            if (user.TryGetValue("status", out var status))
+                                identity.AddClaim(new Claim("status", status.ToString()!));
+                            if (user.TryGetValue("permissions", out var permissions) && permissions is Newtonsoft.Json.Linq.JArray permsArray)
+                            {
+                                foreach (var perm in permsArray)
+                                    identity.AddClaim(new Claim("permission", perm.ToString()!));
+                            }
+                            if (user.TryGetValue("roles", out var roles) && roles is Newtonsoft.Json.Linq.JArray rolesArray)
+                            {
+                                foreach (var role in rolesArray)
+                                    identity.AddClaim(new Claim(ClaimTypes.Role, role.ToString()!));
+                            }
+                        }
+
+                        principal = new ClaimsPrincipal(identity);
+                    }
+                    catch
+                    {
+                        principal = new ClaimsPrincipal(new ClaimsIdentity());
+                    }
                 }
                 catch
                 {
                     principal = new ClaimsPrincipal(new ClaimsIdentity());
                 }
+
             }
 
-            return Task.FromResult(new AuthenticationState(principal));
+            return new AuthenticationState(principal);
         }
     }
 }
