1053aacb14e0847e89658ed22ddd7e3813396e23
Giftson Daniel G <giftsondaniel.g@kryptosinfosys.com>
2025-05-31 23:01:30 +0530

    Remove unnecessary using

--- QLN.Web.Shared/Pages/Content/Daily/DailyFeaturedEventsBase.cs
+++ QLN.Web.Shared/Pages/Content/Daily/DailyFeaturedEventsBase.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using QLN.Common.Infrastructure.DTO_s;
-using static MudBlazor.CategoryTypes;
 
 namespace QLN.Web.Shared.Pages.Content.Daily
 {
