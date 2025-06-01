f8a4f58429d9b5df54508f4bba41754f765e65ea
Grant Vine <grant.vine@crayon.com>
2025-06-01 14:20:42 +0200

    Update for Videos data

---
+++ QLN.Common/DTO's/ContentVideo.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace QLN.Common.Infrastructure.DTO_s
+{
+    public class ContentVideo : ContentBase
+    {
+        [JsonPropertyName("nid")]
+        public string Nid { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [JsonPropertyName("date_created")]
+        public string DateCreated { get; set; }
+
+        [JsonPropertyName("image_url")]
+        public string ImageUrl { get; set; }
+
+        [JsonPropertyName("video_url")]
+        public string VideoUrl { get; set; }
+
+        [JsonPropertyName("user_name")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+    }
+}
