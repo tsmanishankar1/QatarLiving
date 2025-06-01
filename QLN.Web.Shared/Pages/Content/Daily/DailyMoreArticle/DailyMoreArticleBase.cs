fbb79d1745d90859d24486bded678a54911af09e
Giftson Daniel G <giftsondaniel.g@kryptosinfosys.com>
2025-06-01 06:49:18 +0530

    #4627 and #4624 Complete

--- QLN.Web.Shared/Pages/Content/Daily/DailyMoreArticle/DailyMoreArticleBase.cs
+++ QLN.Web.Shared/Pages/Content/Daily/DailyMoreArticle/DailyMoreArticleBase.cs
@@ -2,6 +2,18 @@ using Microsoft.AspNetCore.Components;
 using QLN.Common.Infrastructure.DTO_s;
 public class DailyMoreArticleBase : ComponentBase
 {
+    [Inject] NavigationManager NavigationManager { get; set; }
     [Parameter]
     public List<ContentPost> Items { get; set; } = [];
+
+    protected void NavigatetoArticle()
+    {
+        NavigationManager.NavigateTo("content/events");
+    }
+
+    protected void NavigatetoArticle(ContentPost article)
+    {
+        NavigationManager.NavigateTo($"/article/details/{article.Slug}");
+    }
+
 }
