c926365322f88ce7108a8f15877d64330eacbb22
Vignesh R <vignesh.ra@kryptosinfosys.com>
2025-06-03 11:48:39 +0530

    Register now needs to be hidden Events detail page and Add to Calendar is not matching with Figma. Register Now is Hidden. Change Icon Task #4708 - Daily : In The know Categories should lead to the corresponding category pages. Work Started Task #4693

--- QLN.Web.Shared/Pages/Content/Daily/DailyIntheKnow/DailyIntheKnowBase.cs
+++ QLN.Web.Shared/Pages/Content/Daily/DailyIntheKnow/DailyIntheKnowBase.cs
@@ -17,17 +17,58 @@ namespace QLN.Web.Shared.Pages.Content.Daily.DailyIntheKnow
 
         protected List<CategoryItem> DailyCategories =
         [
-        new CategoryItem { Icon = "/qln-images/content/business_icon.svg", Label = "Business", QLCategLink ="forum/business-finance" },
-        new CategoryItem { Icon = "/qln-images/content/sports_icon.svg", Label = "Sports", QLCategLink ="forum/worldcup" },
-        new CategoryItem { Icon = "/qln-images/content/lifestyle_icon.svg", Label = "Lifestyle", QLCategLink ="forum/fashion" },
-        new CategoryItem { Icon = "/qln-images/content/food_icon.svg", Label = "Food & Dining",QLCategLink ="forum/dining" },
-        new CategoryItem { Icon = "/qln-images/content/travel_icon.svg", Label = "Travel & Leisure", QLCategLink ="travel-tourism"},
+            new CategoryItem
+            {
+                Icon = "/qln-images/content/business_icon.svg",
+                Label = "Business",
+                QLCategLink = "forum/business-finance",
+                CategorySlug = "business"
+            },
+            new CategoryItem
+            {
+                Icon = "/qln-images/content/sports_icon.svg",
+                Label = "Sports",
+                QLCategLink = "forum/worldcup",
+                CategorySlug = "sports"
+            },
+            new CategoryItem
+            {
+                Icon = "/qln-images/content/lifestyle_icon.svg",
+                Label = "Lifestyle",
+                QLCategLink = "forum/fashion",
+                CategorySlug = "lifestyle"
+            },
+            new CategoryItem
+            {
+                Icon = "/qln-images/content/food_icon.svg",
+                Label = "Food & Dining",
+                QLCategLink = "forum/dining",
+                CategorySlug = "lifestyle",
+                SubcategorySlug = "food-dining"
+            },
+            new CategoryItem
+            {
+                Icon = "/qln-images/content/travel_icon.svg",
+                Label = "Travel & Leisure",
+                QLCategLink = "travel-tourism",
+                CategorySlug = "lifestyle",
+                SubcategorySlug = "travel-leisure"
+            }
         ];
 
-        protected void OnClickCommunityCateg(string qlCategLink)
+
+        protected void OnClickCommunityCateg(CategoryItem item)
         {
-            NavigationManager.NavigateTo("/content/news");
+            var uri = $"/content/news?category={item.CategorySlug}";
+
+            if (!string.IsNullOrEmpty(item.SubcategorySlug))
+            {
+                uri += $"&subcategory={item.SubcategorySlug}";
+            }
+
+            NavigationManager.NavigateTo(uri);
         }
+
 
         protected void OnClickCommunityViewAll()
         {
