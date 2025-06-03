2f810f83a1f36b0f4b6839c7078e1ea495259ba4
Jaswanth R <jaswanth.r@kryptosinfosys.com>
2025-06-03 17:25:41 +0530

    comment logged in state

--- QLN.Web.Shared/Pages/Content/Community/CommentSectionBase.cs
+++ QLN.Web.Shared/Pages/Content/Community/CommentSectionBase.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
 using Microsoft.JSInterop;
 using MudBlazor;
+using QLN.Web.Shared.Contracts;
 using QLN.Web.Shared.Model;
+using QLN.Web.Shared.Models;
+using QLN.Web.Shared.Services;
+using System.Net.NetworkInformation;
+using System.Security.Claims;
 
 
 namespace QLN.Web.Shared.Pages.Content.Community
 {
     public class CommentSectionBase : ComponentBase
     {
+
         [Parameter]
         public PostModel Comment { get; set; }
         protected string newComment = string.Empty;
@@ -21,16 +28,37 @@ namespace QLN.Web.Shared.Pages.Content.Community
         protected bool isMenuOpen = false;
         protected bool IsLiked { get; set; } = false;
 
-        [Inject] private IJSRuntime JS { get; set; }
-
+        [Inject] protected IJSRuntime JS { get; set; }
+        [Inject] protected CookieAuthStateProvider CookieAuthenticationStateProvider { get; set; }
+        [Inject] protected ICommunityService CommunityService { get; set; }
+        [Inject] protected ISnackbar Snackbar { get; set; }
 
         protected MudTextField<string> multilineReference;
 
 
 
         private MudTextField<string> textFieldRef;
+      
 
-       
+        private NavigationPath navigationPath;
+        public string Name { get; set; } = string.Empty;
+        public int CurrentUserId { get; set; }
+        public bool IsLoggedIn { get; set; } = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await CookieAuthenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            if (user.Identity?.IsAuthenticated == true)
+            {
+                Name = user.FindFirst(ClaimTypes.Name)?.Value ?? string.Empty;
+                CurrentUserId = int.TryParse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value, out var uid) ? uid : 0;
+                Console.WriteLine($"Current User: {CurrentUserId}");
+                IsLoggedIn = true;
+            }
+        }
+
 
 
         protected void OnMenuToggle(bool open)
