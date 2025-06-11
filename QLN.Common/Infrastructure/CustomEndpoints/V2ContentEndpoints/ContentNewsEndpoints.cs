7271f05bbcdf511c4f32407de771b2fe7706b3a1
sangeethkumar.r <sangeethkumar.r@KTP-RLTT-KIT030>
2025-06-11 16:03:54 +0530

    updated the dto

--- QLN.Common/Infrastructure/CustomEndpoints/V2ContentEndpoints/ContentNewsEndpoints.cs
+++ QLN.Common/Infrastructure/CustomEndpoints/V2ContentEndpoints/ContentNewsEndpoints.cs
@@ -8,5 +8,9 @@ namespace QLN.Common.Infrastructure.CustomEndpoints.V2ContentEndpoints
 {
     internal class ContentNewsEndpoints
     {
+        private const int V2CATEGORY_CACHE_EXPIRY_IN_MINS = 60;
+        private const int V2NEWS_CACHE_EXPIRY_IN_MINS = 5;
+        private const int V2COMMUNITY_CACHE_EXPIRY_IN_MINS = 10;
+        private const int V2EVENTS_CACHE_EXPIRY_IN_MINS = 10;
     }
 }
---
+++ QLN.Common/Infrastructure/IService/V2IContent/V2IcontentNews.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLN.Common.Infrastructure.IService.V2IContent
+{
+    internal class V2IcontentNews
+    {
+    }
+}
