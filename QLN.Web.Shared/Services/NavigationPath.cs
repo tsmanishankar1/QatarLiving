cd8cc737c0564a85f828b456e4b6723086aa18e9
Grant Vine <grant.vine@crayon.com>
2025-06-16 09:43:50 +0200

    Fix for user links

--- QLN.Web.Shared/Services/NavigationPath.cs
+++ QLN.Web.Shared/Services/NavigationPath.cs
@@ -71,9 +71,10 @@
         public string RewardsProfileMenu { get; set; } = "https://qlr.qatarliving.com/en/rewards/dashboard";
         public string MySavedSearchesProfileMenu { get; set; } =  "https://qlv.qatarliving.com/en/vehicles/favourites?tab=saved-searches";
 
-        public string ClassifiedsProfileMenu { get; set; } = "https://www.qatarliving.com/user/<USER_ID>/classifieds";
-        public string JobsProfileMenu { get; set; } = "https://www.qatarliving.com/user/<USER_ID>/jobs";
-         public string MyProfileMenu { get; set; } = "https://www.qatarliving.com/user/<USER_ID>";
+        public string ClassifiedsProfileMenu { get; set; } = "https://www.qatarliving.com//classifieds/dashboard/<ALIAS>";
+        public string ServicesProfileMenu { get; set; } = "https://www.qatarliving.com/services/dashboard/<ALIAS>";
+        public string JobsProfileMenu { get; set; } = "https://www.qatarliving.com/user/<USER_ID>/listings";
+        public string MyProfileMenu { get; set; } = "https://www.qatarliving.com/user/<USER_ID>";
         public string MySavedAdsProfileMenu { get; set; } = "https://www.qatarliving.com/user/<USER_ID>/wishlist";
         public string ManageMyAdsProfileMenu { get; set; } = "https://www.qatarliving.com/user/<USER_ID>/classifieds";
 
