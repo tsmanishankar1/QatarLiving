e034f4c5e439cc5b5dd3c17d9216a24e7d283c4b
Jaswanth R <jaswanth.r@kryptosinfosys.com>
2025-06-16 15:44:43 +0530

    #3804 -FE Items Dashboard - Summary & Statistics Section Design and API Integration

--- QLN.Web.Shared/Pages/Classifieds/Dashboards/ItemDashboardBase.cs
+++ QLN.Web.Shared/Pages/Classifieds/Dashboards/ItemDashboardBase.cs
@@ -1,14 +1,31 @@
 ﻿using Microsoft.AspNetCore.Components;
 using QLN.Web.Shared.Components.BreadCrumb;
+using QLN.Web.Shared.Models;
+using QLN.Web.Shared.Services.Interface;
+using static QLN.Web.Shared.Pages.Subscription.SubscriptionDetails;
 
 namespace QLN.Web.Shared.Pages.Classifieds.Dashboards
 {
     public class ItemDashboardBase : ComponentBase
     {
 
-        [Inject] private NavigationManager Navigation { get; set; } = default!;
+        [Inject] protected NavigationManager Navigation { get; set; } = default!;
+        [Inject] protected IClassifiedDashboardService ClassfiedDashboardService { get; set; }
+
 
         protected List<BreadcrumbItem> breadcrumbItems = new();
+        protected List<StatItem> stats = new();
+
+        protected int _activeTabIndex;
+        protected bool _isChecked = false;
+        protected int _selectedAdsTab = 0;
+
+        protected string? _errorMessage;
+        protected BusinessProfile? _businessProfile;
+        protected List<AdModal> publishedAds = new();
+        protected List<AdModal> unpublishedAds = new();
+        protected bool _isLoading { get; set; } = true;
+        private string _authToken;
 
         protected override void OnInitialized()
         {
@@ -18,5 +35,90 @@ namespace QLN.Web.Shared.Pages.Classifieds.Dashboards
                 new() { Label = "Dashboard", Url = "/qln/classified/dashboard/items", IsLast = true }
             };
         }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6Ijk3NTQ1NGI1LTAxMmItNGQ1NC1iMTUyLWUzMGYzNmYzNjNlMiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJNVUpBWSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6Im11amF5LmFAa3J5cHRvc2luZm9zeXMuY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbW9iaWxlcGhvbmUiOiIrOTE3NzA4MjA0MDcxIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjpbIlVzZXIiLCJTdWJzY3JpYmVyIl0sIlVzZXJJZCI6Ijk3NTQ1NGI1LTAxMmItNGQ1NC1iMTUyLWUzMGYzNmYzNjNlMiIsIlVzZXJOYW1lIjoiTVVKQVkiLCJFbWFpbCI6Im11amF5LmFAa3J5cHRvc2luZm9zeXMuY29tIiwiUGhvbmVOdW1iZXIiOiIrOTE3NzA4MjA0MDcxIiwiZXhwIjoxNzUwMDcxODA2LCJpc3MiOiJRYXRhciBMaXZpbmciLCJhdWQiOiJRYXRhciBMaXZpbmcifQ.JCtLQNDciDIMyTgpZBYMMP8xmHrqNOHLKBq1igOOj2U";
+
+                await LoadSubscriptionDetailsAsync(3);
+                SetHardcodedBusinessProfile();
+                StateHasChanged();
+
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+
+        protected void SetHardcodedBusinessProfile()
+        {
+            _businessProfile = new BusinessProfile
+            {
+                Name = "Luxury Store",
+                CategoryName = "Preloved",
+                Duration = "6 month Plus",
+                ValidFrom = "2025-04-27",
+                ValidTo = "2025-10-27",
+                LogoUrl = "qln-images/subscription/CompanyLogo.svg"
+            };
+            StateHasChanged();
+        }
+        protected async Task LoadSubscriptionDetailsAsync(int verticalId)
+        {
+            _isLoading = true;
+
+            try
+            {
+                var response = await ClassfiedDashboardService.GetItemDashboard(_authToken);
+
+                if (response?.ItemsDashboard != null)
+                {
+                    stats = new List<StatItem>
+            {
+                new() { Title = "Published Ads", Value = $"{response.ItemsDashboard.PublishedAds}", Icon = "PublishedAds.svg" },
+                new() { Title = "Promoted Ads", Value = $"{response.ItemsDashboard.PromotedAds}", Icon = "PromotedAds.svg" },
+                new() { Title = "Featured Ads", Value = $"{response.ItemsDashboard.FeaturedAds}", Icon = "FeaturedAds.svg" },
+                new() { Title = "Refreshes", Value = $"{response.ItemsDashboard.Refreshes} / {response.ItemsDashboard.TotalAllowedRefreshes}", Icon = "Refreshes.svg" },
+                new() { Title = "Impressions", Value = $"{response.ItemsDashboard.Impressions:N0}", Icon = "Impressions.svg" },
+                new() { Title = "Views", Value = $"{response.ItemsDashboard.Views:N0}", Icon = "Views.svg" },
+                new() { Title = "WhatsApp", Value = $"{response.ItemsDashboard.WhatsAppClicks}", Icon = "WhatsApp.svg" },
+                new() { Title = "Calls", Value = $"{response.ItemsDashboard.Calls}", Icon = "Calls.svg" },
+            };
+                    publishedAds = response.ItemsAds?.PublishedAds ?? new();
+
+                }
+                else
+                {
+                    _errorMessage = "No subscription details found.";
+                    stats = new();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error loading subscription details: " + ex.Message);
+                _errorMessage = "Something went wrong while loading subscription stats.";
+                stats = new();
+            }
+            finally
+            {
+                _isLoading = false;
+
+            }
+        }
+        protected void SetTab(int index)
+        {
+            _activeTabIndex = index;
+        }
+        protected void NavigateToEditProfile()
+        {
+            Navigation.NavigateTo("/edit-company");
+        }
+        protected void NavigateToAdPost()
+        {
+            Navigation.NavigateTo("/classifieds/createform");
+        }
+
     }
 }
