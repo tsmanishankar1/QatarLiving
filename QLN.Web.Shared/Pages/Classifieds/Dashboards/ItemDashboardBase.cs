1652298edb20202b1b14869c1f7234fff9d6ec88
Jaswanth R <jaswanth.r@kryptosinfosys.com>
2025-06-16 13:18:32 +0530

    Item Dashboard init

---
+++ QLN.Web.Shared/Pages/Classifieds/Dashboards/ItemDashboardBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using QLN.Web.Shared.Components.BreadCrumb;
+
+namespace QLN.Web.Shared.Pages.Classifieds.Dashboards
+{
+    public class ItemDashboardBase : ComponentBase
+    {
+
+        [Inject] private NavigationManager Navigation { get; set; } = default!;
+
+        protected List<BreadcrumbItem> breadcrumbItems = new();
+
+        protected override void OnInitialized()
+        {
+            breadcrumbItems = new()
+            {
+                new() { Label = "Classifieds", Url = "qln/classifieds" },
+                new() { Label = "Dashboard", Url = "/qln/classified/dashboard/items", IsLast = true }
+            };
+        }
+    }
+}
