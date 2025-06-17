b784b2df839883aaeadeb328a9e0fe7570e7bb90
ashwin.g <ashwin.g@kryptosinfosys.com>
2025-06-17 16:19:56 +0530

    Deals Detail Page Static UI development #5319 Deals Carousel Page #5457

---
+++ QLN.Web.Shared/Pages/Classifieds/Deals/Components/DealsPreviewBase.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components;
+using QLN.Common.Infrastructure.DTO_s;
+
+namespace QLN.Web.Shared.Pages.Classifieds.Deals.Components
+{
+    public class DealsPreviewBase : ComponentBase
+    {
+        public List<string> imageUrls = new()
+        {
+            "/qln-images/classifieds/deals_preview_image1.svg",
+            "/qln-images/classifieds/deals_preview_image2.svg",
+            "/qln-images/classifieds/deals_preview_image3.svg",
+        };
+        public int currentIndex = 0;
+
+    public void PreviousImage()
+    {
+        if (currentIndex > 0)
+            currentIndex--;
+    }
+
+    public void NextImage()
+    {
+        if (currentIndex < imageUrls.Count - 1)
+            currentIndex++;
+    }
+    }
+}
