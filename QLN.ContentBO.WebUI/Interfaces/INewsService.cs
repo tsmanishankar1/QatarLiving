47e41f5ac97501c10f8da21b78329f4359825d28
Giftson Daniel G <giftsondaniel.g@kryptosinfosys.com>
2025-06-20 10:53:30 +0530

    Configure Base #5829

--- QLN.ContentBO.WebUI/Interfaces/INewsService.cs
+++ QLN.ContentBO.WebUI/Interfaces/INewsService.cs
@@ -1,6 +1,11 @@
-﻿namespace QLN.ContentBO.WebUI.Interfaces
+﻿
+using QLN.ContentBO.WebUI.Models;
+
+namespace QLN.ContentBO.WebUI.Interfaces
 {
     public interface INewsService
     {
+        Task<HttpResponseMessage> GetAllArticle(int id);
+        Task<HttpResponseMessage> CreateArticle(V2ContentNewsDto newsArticle);
     }
 }
--- QLN.ContentBO.WebUI/Services/NewsService.cs
+++ QLN.ContentBO.WebUI/Services/NewsService.cs
@@ -1,8 +1,47 @@
 ﻿using QLN.ContentBO.WebUI.Interfaces;
+using QLN.ContentBO.WebUI.Models;
+using QLN.ContentBO.WebUI.Services.Base;
+using System.Net;
+using System.Text;
+using System.Text.Json;
 
 namespace QLN.ContentBO.WebUI.Services
 {
-    public class NewsService : INewsService
+    public class NewsService : ServiceBase<NewsService>, INewsService
     {
+        public NewsService(HttpClient httpClientDI, ILogger<NewsService> Logger)
+           : base(httpClientDI, Logger)
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> CreateArticle(V2ContentNewsDto newsArticle)
+        {
+            try
+            {
+                var newsArticleJson = new StringContent(JsonSerializer.Serialize(newsArticle), Encoding.UTF8, "application/json");
+                var response = await PostAsync("api/CreateArticle", newsArticleJson);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "CreateArticle");
+                return new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+            }
+        }
+
+        public async Task<HttpResponseMessage> GetAllArticle(int id)
+        {
+            try
+            {
+                var response = await GetAsync($"api/GetAllArticles");
+                return response;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "GetAllArticle");
+                return new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+            }
+        }
     }
 }
