d62281b2b9d217574c2d0246ddfad4e0ef884083
Giftson Daniel G <giftsondaniel.g@kryptosinfosys.com>
2025-06-23 09:43:12 +0530

    Mock API Service #6198

--- QLN.ContentBO.WebUI/Components/News/AddArticleBase.cs
+++ QLN.ContentBO.WebUI/Components/News/AddArticleBase.cs
@@ -12,18 +12,19 @@ namespace QLN.ContentBO.WebUI.Components.News
         [Inject] INewsService newsService { get; set; }
         [Inject] ILogger<AddArticleBase> Logger { get; set; }
         [Inject] IJSRuntime JS { get; set; }
-        protected V2ContentNewsDto article { get; set; } = new();
+        protected NewsArticleDTO article { get; set; } = new();
 
-        protected List<string> Categories = [];
-        protected List<string> Subcategories = [];
-        protected List<string> Slots = [];
-        protected List<string> Writers = [];
+        protected List<NewsCategory> Categories = [];
+        protected List<Slot> Slots = [];
+        protected List<string> WriterTags = [];
 
         protected MudExRichTextEdit Editor;
 
         protected override async Task OnInitializedAsync()
         {
-
+            Categories = await GetNewsCategories();
+            Slots = await GetSlots();
+            WriterTags = await GetWriterTags();
         }
 
         protected async Task HandleValidSubmit()
@@ -61,6 +62,63 @@ namespace QLN.ContentBO.WebUI.Components.News
         protected void Cancel()
         {
 
+        }
+
+        private async Task<List<NewsCategory>> GetNewsCategories()
+        {
+            try
+            {
+                var apiResponse = await newsService.GetNewsCategories();
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    return await apiResponse.Content.ReadFromJsonAsync<List<NewsCategory>>() ?? [];
+                }
+
+                return [];
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "GetNewsCategories");
+                return [];
+            }
+        }
+
+        private async Task<List<Slot>> GetSlots()
+        {
+            try
+            {
+                var apiResponse = await newsService.GetSlots();
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    return await apiResponse.Content.ReadFromJsonAsync<List<Slot>>() ?? [];
+                }
+
+                return [];
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "GetSlots");
+                return [];
+            }
+        }
+
+        private async Task<List<string>> GetWriterTags()
+        {
+            try
+            {
+                var apiResponse = await newsService.GetWriterTags();
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    return await apiResponse.Content.ReadFromJsonAsync<List<string>>() ?? [];
+                }
+
+                return [];
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "GetWriterTags");
+                return [];
+            }
         }
     }
 }
---
+++ QLN.ContentBO.WebUI/Models/ArticleCategory.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QLN.ContentBO.WebUI.Models
+{
+    public class ArticleCategory
+    {
+        [Required]
+        public int CategoryId { get; set; }
+        
+        [Required]
+        public int SubcategoryId { get; set; }
+
+        /// <summary>
+        /// Defaults to UnPublished Slot
+        /// </summary>
+        public int SlotId { get; set; }
+    }
+}
