2dda0c9ca8ce65e8d2a66d35a798aa691a6666cc
ashwin.g <ashwin.g@kryptosinfosys.com>
2025-06-24 08:27:38 +0530

    Added Auto complete field

--- QLN.ContentBO.WebUI/Pages/EventCreateForm/MessageBox/MessageBoxBase.cs
+++ QLN.ContentBO.WebUI/Pages/EventCreateForm/MessageBox/MessageBoxBase.cs
@@ -4,14 +4,29 @@ using MudBlazor;
 public class MessageBoxBase : ComponentBase
 {
     [Parameter] public string Title { get; set; } = "Featured Event";
-    [Parameter] public string Placeholder { get; set; }
+    [Parameter] public string Placeholder { get; set; } = "Article Title*";
     [Parameter] public EventCallback<string> OnAdd { get; set; }
-    protected string EventTitle;
+    [Parameter]
+    public List<string> AllEventTitles { get; set; } = new();
+    protected string SelectedValue { get; set; }
 
     protected async Task AddClicked()
     {
         if (OnAdd.HasDelegate)
-            await OnAdd.InvokeAsync(EventTitle);
+            await OnAdd.InvokeAsync(SelectedValue);
     }
-   
+    protected Task<IEnumerable<string>> SearchEventTitles(string value, CancellationToken token)
+    {
+        IEnumerable<string> result;
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            result = AllEventTitles;
+        }
+        else
+        {
+            result = AllEventTitles
+                .Where(e => e.Contains(value, StringComparison.InvariantCultureIgnoreCase));
+        }
+        return Task.FromResult(result);
+    }
 }
