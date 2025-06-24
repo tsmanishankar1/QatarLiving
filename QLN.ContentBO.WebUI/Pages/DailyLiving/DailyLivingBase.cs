3fad1ff3945a59ba0f36f4b9ff09d85df1718f9e
ashwin.g <ashwin.g@kryptosinfosys.com>
2025-06-24 19:37:57 +0530

    Content Back Office Layout Design

--- QLN.ContentBO.WebUI/Pages/DailyLiving/DailyLivingBase.cs
+++ QLN.ContentBO.WebUI/Pages/DailyLiving/DailyLivingBase.cs
@@ -3,11 +3,24 @@ using Microsoft.AspNetCore.Components.Forms;
 using MudExRichTextEditor;
 using QLN.Common.Infrastructure.DTO_s;
 using MudBlazor;
-using QLN.ContentBO.WebUI.Pages.EventCreateForm.MessageBox;
+using QLN.ContentBO.WebUI.Pages.DailyLiving.Components.RadioAutoCompleteDialog;
 namespace QLN.ContentBO.WebUI.Pages
 {
     public class DailyLivingBase : ComponentBase
     {
         protected int activeIndex = 0;
+        [Inject]
+        public IDialogService DialogService { get; set; }
+        protected Task OpenDialogAsync()
+        {
+            var options = new DialogOptions
+            {
+                MaxWidth = MaxWidth.Small,
+                FullWidth = true,
+                CloseOnEscapeKey = true
+            };
+            return DialogService.ShowAsync<RadioAutoCompleteDialog>(string.Empty, options);
+        }
+
     }
 }
