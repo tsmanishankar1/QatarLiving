70d8b0cebf67087078e7ebd6f59ed6eb7d047775
Giftson Daniel G <giftsondaniel.g@kryptosinfosys.com>
2025-06-26 15:18:10 +0530

    Import Leaflet JS

---
+++ QLN.Web.Shared/wwwroot/leafletMap.js
@@ -0,0 +1,25 @@
+﻿window.initMap = (lat, lng) => {
+    const map = L.map('map').setView([lat, lng], 16);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    const customIcon = L.icon({
+        iconUrl: '/qln-images/location_pin.svg',
+        iconSize: [32, 48],
+        iconAnchor: [16, 48],
+        popupAnchor: [0, -40],
+        shadowUrl: null
+    });
+
+    const marker = L.marker([lat, lng], {
+        icon: customIcon,
+        draggable: true
+    }).addTo(map);
+
+    marker.on('dragend', function (e) {
+        const newPos = e.target.getLatLng();
+        DotNet.invokeMethodAsync('QLN.Web.Shared', 'UpdateLatLng', newPos.lat, newPos.lng);
+    });
+};
