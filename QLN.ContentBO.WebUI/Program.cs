7ead7e6bf5e1c9f8e85c2cc21a094a96bd1612b0
Grant Vine <grant.vine@crayon.com>
2025-06-29 16:03:23 +0200

    Updates for deployment

--- QLN.ContentBO.WebUI/Program.cs
+++ QLN.ContentBO.WebUI/Program.cs
@@ -1,56 +1,43 @@
 using MudBlazor.Services;
-using NLog;
-using NLog.Web;
 using QLN.ContentBO.WebUI.Interfaces;
 using QLN.ContentBO.WebUI.MockServices;
 using QLN.ContentBO.WebUI.Services;
 
-// Early init of NLog to allow startup and exception logging, before host is built
-var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
-logger.Debug("init main");
-try
+var builder = WebApplication.CreateBuilder(args);
+var contentBOAPIURL = builder.Configuration["ServiceUrlPaths:ContentBOAPI"];
+if (string.IsNullOrWhiteSpace(contentBOAPIURL))
 {
-    var builder = WebApplication.CreateBuilder(args);
-    var contentBOAPIURL = builder.Configuration["ServiceUrlPaths:ContentBOAPI"];
-    if (string.IsNullOrWhiteSpace(contentBOAPIURL))
-    {
-        throw new InvalidOperationException("Content Back Office API URL is missing in the configuration file.");
-    }
-
-    // Add services to the container.
-    builder.Services.AddRazorPages();
-    builder.Services.AddServerSideBlazor();
-    builder.Services.AddMudServices();
-
-    builder.Services.AddHttpClient<INewsService, NewsMockService>(client =>
-    {
-        client.BaseAddress = new Uri(contentBOAPIURL);
-    });
-
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (!app.Environment.IsDevelopment())
-    {
-        app.UseExceptionHandler("/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.MapStaticAssets();
-
-    app.UseRouting();
-
-    app.MapBlazorHub();
-    app.MapFallbackToPage("/_Host");
-
-    app.Run();
+    throw new InvalidOperationException("Content Back Office API URL is missing in the configuration file.");
 }
-catch (Exception exception)
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
+
+builder.Services.AddHttpClient<INewsService, NewsMockService>(client =>
 {
-    // NLog: catch setup errors
-    logger.Error(exception, "Stopped program because of exception");
-    throw;
+    client.BaseAddress = new Uri(contentBOAPIURL);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
+app.MapStaticAssets();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
+
