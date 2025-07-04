a3f5e83f51c0494a2540155f30157baaf3762781
Petchiappan.P <Petchiappan.P@kryptosinfosys.com>
2025-07-04 21:29:05 +0530

    updated code

--- QLN.Common/Infrastructure/IService/V2IContent/IV2CommunityPostService.cs
+++ QLN.Common/Infrastructure/IService/V2IContent/IV2CommunityPostService.cs
@@ -15,6 +15,7 @@ namespace QLN.Common.Infrastructure.IService.V2IContent
         Task<ForumCategoryListDto> GetAllForumCategoriesAsync(CancellationToken cancellationToken = default);
         Task<bool> SoftDeleteCommunityPostAsync(Guid postId, string userId, CancellationToken ct = default);
         Task<PaginatedCommunityPostResponseDto> GetAllCommunityPostsAsync(string? categoryId = null, string? search = null, int? page = null, int? pageSize = null, string? sortDirection = null, CancellationToken ct = default);
+     
         Task<V2CommunityPostDto?> GetCommunityPostByIdAsync(Guid id, CancellationToken ct = default);
         Task<bool> LikePostForUser(CommunityPostLikeDto dto, CancellationToken ct = default);
         Task AddCommentToCommunityPostAsync(CommunityCommentDto dto, CancellationToken ct = default);
