08b7adf494598f4e291ce7e58cb2c18a0b95f8a9
MUJAY A <mujay.a@kryptosinfosys.com>
2025-07-05 18:35:52 +0530

    completed the Post comments API, GetCommentsByArticleId with pagination, comment count and like count for that comment and completed the likeNewsComment

---
+++ QLN.Common/DTO's/V2NewsCommentDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLN.Common.DTO_s
+{
+    public class V2NewsCommentDto
+    {
+        public string Nid { get; set; }             // Article ID
+        public string Uid { get; set; }             // User ID
+        public string? UserName { get; set; }    // User Name
+        public string Comment { get; set; } = "";
+        public Guid CommentId { get; set; } = Guid.NewGuid();
+        public DateTime CommentedAt { get; set; } = DateTime.UtcNow;
+    }
+
+    public class NewsCommentApiResponse
+    {
+        public string Status { get; set; } = "success";
+        public string Message { get; set; } = "Comment saved successfully.";
+    }
+}
