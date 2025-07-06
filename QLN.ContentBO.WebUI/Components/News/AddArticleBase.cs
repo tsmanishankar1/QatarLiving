6710a67ee3cf2b9ebffe2dde78c5cf70d4d48b71
giftsondaniel.g <giftsondaniel.g@kryptosinfosys.com>
2025-07-06 16:53:25 +0530

    Article Content is required

--- QLN.ContentBO.WebUI/Components/News/AddArticleBase.cs
+++ QLN.ContentBO.WebUI/Components/News/AddArticleBase.cs
@@ -95,7 +95,7 @@ namespace QLN.ContentBO.WebUI.Components.News
                     Snackbar.Add("Image is required", severity: Severity.Error);
                     return;
                 }
-                if (string.IsNullOrEmpty(article.Content) || string.IsNullOrWhiteSpace(article.Content))
+                if (string.IsNullOrEmpty(article.Content) || string.IsNullOrWhiteSpace(article.Content) || article.Content == "<p></p>" || article.Content == "<p> </p>")
                 {
                     Snackbar.Add("Article Content is required", severity: Severity.Error);
                     return;
