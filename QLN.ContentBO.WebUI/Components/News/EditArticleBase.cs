99604e3255ca00137ac3d6b0d409525716c527e7
giftsondaniel.g <giftsondaniel.g@kryptosinfosys.com>
2025-07-06 16:49:43 +0530

    Fix Validations

--- QLN.ContentBO.WebUI/Components/News/EditArticleBase.cs
+++ QLN.ContentBO.WebUI/Components/News/EditArticleBase.cs
@@ -65,10 +65,6 @@ namespace QLN.ContentBO.WebUI.Components.News
 
         protected void AddCategory()
         {
-            if (Category.SlotId == 0)
-            {
-                Category.SlotId = 15; 
-            }
             if (Category.CategoryId == 0 || Category.SubcategoryId == 0)
             {
                 Snackbar.Add("Category and Sub Category is required", severity: Severity.Normal);
@@ -79,6 +75,10 @@ namespace QLN.ContentBO.WebUI.Components.News
                 Snackbar.Add("Maximum of 2 Category and Sub Category combinations are allowed", severity: Severity.Normal);
                 Category = new();
                 return;
+            }
+            if (Category.SlotId == 0)
+            {
+                Category.SlotId = 15; // By Default UnPublished.
             }
             TempCategoryList.Add(Category);
             Category = new();
