092d47b2d5367e54f2bd25390206b3e9991d7071
giftsondanielg <giftsondaniel.g@kryptosinfosys.com>
2025-07-12 16:15:36 +0530

    Format Date Correctly

--- QLN.Web.Shared/Models/NewsArticleExtensions.cs
+++ QLN.Web.Shared/Models/NewsArticleExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using QLN.Common.Infrastructure.DTO_s;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,10 @@ namespace QLN.Web.Shared.Models
     {
         public static ContentPost ToContentPost(this NewsArticleDTO article)
         {
+            // Convert to GMT+1
+            var gmtPlus1 = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"); 
+            var publishedDate = TimeZoneInfo.ConvertTimeFromUtc(article.PublishedDate == default ? article.CreatedAt : article.PublishedDate, gmtPlus1);
+
             return new ContentPost
             {
                 Id = article.Id,
@@ -21,14 +26,15 @@ namespace QLN.Web.Shared.Models
                 UpdatedBy = Guid.TryParse(article.UpdatedBy, out var ub) ? ub : null,
                 UpdatedAt = article.UpdatedAt,
 
-                Nid = article.Slug,
-                DateCreated = article.CreatedAt.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+                Nid = article.Id.ToString(),
+                DateCreated = publishedDate.ToString("MMMM d, yyyy 'at' h:mm tt 'GMT+1'", CultureInfo.InvariantCulture),
                 ImageUrl = article.CoverImageUrl ?? string.Empty,
                 UserName = article.authorName,
-                ForumId = article.Categories.FirstOrDefault()?.CategoryId.ToString(),
+                ForumId = string.Empty,
                 Title = article.Title,
                 Description = article.Content,
-                Category = article.WriterTag,
+                Category = string.Empty,
+                Slug = article.Slug,
                 Comments = []
             };
         }
