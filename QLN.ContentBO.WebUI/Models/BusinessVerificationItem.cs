e119d7c79dc83e8a8707df3f11a77f785a169195
Vignesh R <vignesh.ra@kryptosinfosys.com>
2025-07-17 11:16:45 +0530

    user Verification listing done

---
+++ QLN.ContentBO.WebUI/Models/BusinessVerificationItem.cs
@@ -0,0 +1,11 @@
+namespace QLN.ContentBO.WebUI.Models
+{
+    public class BusinessVerificationItem
+    {
+        public string BusinessName { get; set; }
+        public string UserName { get; set; }
+        public string CRFile { get; set; }
+        public string CRLicense { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
