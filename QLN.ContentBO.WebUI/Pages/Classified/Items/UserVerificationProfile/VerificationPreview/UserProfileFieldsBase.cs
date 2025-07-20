a17c8c1d1c72293c6f21284b5f82bdc3d215f738
Vignesh R <vignesh.ra@kryptosinfosys.com>
2025-07-20 11:27:47 +0530

    user-profile-fields updated

---
+++ QLN.ContentBO.WebUI/Pages/Classified/Items/UserVerificationProfile/VerificationPreview/UserProfileFieldsBase.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Components;
+
+namespace QLN.ContentBO.WebUI.Pages.Classified.Items.UserVerificationProfile.VerificationPreview
+{
+    public class UserProfileFieldsBase : ComponentBase
+    {
+        // Basic Info
+        protected string CompanyName => "Lulu Hypermarket";
+        protected string Country => "Qatar";
+        protected string City => "Doha";
+        protected string Location => "West Bay";
+        protected string PhoneNumber => "+974 2234 5675";
+        protected string WhatsAppNumber => "+974 2234 5675";
+        protected string WebsiteUrl => "www.website.com";
+
+        // Operating Hours
+        protected string StartDay => "Sunday";
+        protected string EndDay => "Thursday";
+        protected string StartHour => "8:00 AM";
+        protected string EndHour => "5:00 PM";
+
+        // Company Profile
+        protected string BusinessNature => "Hypermarket";
+        protected string CompanySize => "500+";
+        protected string CompanyType => "MNC";
+        protected string UserDesignation => "Social Media Manager";
+        protected string BusinessDescription => "For sale is a gently used White Google Pixel 6 Pro XL, a top-of-the-line smartphone...";
+
+        // License & Visibility
+        protected string CRNumber => "202025";
+        protected string CompanyReferenceFileName => "company_cr_2025.pdf";
+        protected string CompanyReferenceUrl => $"/files/company_cr_2025.pdf";
+    }
+}
