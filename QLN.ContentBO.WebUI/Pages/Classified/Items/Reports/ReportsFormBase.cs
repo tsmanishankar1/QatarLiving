e21137906fd83bbf03d9234b943afedb1122436c
Vignesh R <vignesh.ra@kryptosinfosys.com>
2025-07-20 15:54:43 +0530

    report ui is done

---
+++ QLN.ContentBO.WebUI/Pages/Classified/Items/Reports/ReportsFormBase.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace QLN.ContentBO.WebUI.Pages.Classified.Items.Reports
+{
+
+    public class ReportsFormBase : ComponentBase
+    {
+        protected List<string> _categories = new() { "Sales", "Marketing", "HR", "IT" };
+        protected List<string> _statuses = new() { "Active", "Inactive", "Pending" };
+        protected List<string> _subcategories = new() { "Q1", "Q2", "Q3", "Q4" };
+
+        protected string _selectedCategory;
+        protected string _selectedStatus;
+        protected string _selectedSubcategory;
+
+        // Date range logic
+       protected DateRange _dateRange = new(); // both Start and End are null by default
+        protected DateRange _tempDateRange = new();
+
+        protected bool showDatePopover = false;
+
+        protected void ToggleDatePopover()
+        {
+            _tempDateRange = new DateRange(_dateRange.Start, _dateRange.End);
+            showDatePopover = !showDatePopover;
+        }
+
+        protected void CancelDatePopover()
+        {
+            showDatePopover = false;
+        }
+
+        protected void ApplyDatePopover()
+        {
+            _dateRange = new DateRange(_tempDateRange.Start, _tempDateRange.End);
+            showDatePopover = false;
+            StateHasChanged();
+        }
+    }
+}
