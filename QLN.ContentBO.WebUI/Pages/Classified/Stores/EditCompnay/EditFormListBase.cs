9ac6abc222bad6dc2921bec028fa06381628b156
Vignesh R <vignesh.ra@kryptosinfosys.com>
2025-07-23 11:58:44 +0530

    edit form file updated

--- QLN.ContentBO.WebUI/Pages/Classified/Stores/EditCompnay/EditFormListBase.cs
+++ QLN.ContentBO.WebUI/Pages/Classified/Stores/EditCompnay/EditFormListBase.cs
@@ -22,6 +22,12 @@ namespace QLN.ContentBO.WebUI.Pages.Classified.Stores.EditCompnay
         [Inject]
         public NavigationManager NavigationManager { get; set; } = default!;
 
+        protected string ShortFileName(string name, int max)
+         {
+                if (string.IsNullOrEmpty(name)) return "";
+                return name.Length <= max ? name : name.Substring(0, max) + "...";
+        }
+
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
