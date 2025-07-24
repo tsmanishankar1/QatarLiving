a41507fef4b49fa80f52fb7db5957cffb09e21bf
Kousiki S <kousiki.s@kryptosinfosys.com>
2025-07-24 19:54:30 +0530

    I have implemented notification service using pubsub.

---
+++ QLN.Notification.MS/CustomEndpoints/NotificationEndpoints/NotificationEndpointDependency.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLN.Notification.MS.CustomEndpoints.NotificationEndpoints
+{
+    public static class NotificationEndpointDependency
+    {
+        public static RouteGroupBuilder MapNotificationEndpoints(this RouteGroupBuilder group)
+        {
+            group.MapNotificationSubscriber();
+            return group;
+        }
+    }
+}
