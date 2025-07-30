3910a04ec930f90efae5bd8f080501afa2d3ea6d
giftsondanielg <giftsondaniel.g@kryptosinfosys.com>
2025-07-30 16:08:12 +0530

    Basic Image Paste Handler #11191

---
+++ QLN.ContentBO.WebUI/wwwroot/imagePasteHandler.js
@@ -0,0 +1,21 @@
+﻿window.pasteImageHandler = {
+    register: function (elementId, dotNetRef) {
+        const el = document.getElementById(elementId);
+        if (!el) return;
+
+        el.addEventListener('paste', function (event) {
+            const items = (event.clipboardData || event.originalEvent.clipboardData).items;
+
+            for (const item of items) {
+                if (item.type.indexOf("image") === 0) {
+                    const blob = item.getAsFile();
+                    const reader = new FileReader();
+                    reader.onload = function (event) {
+                        dotNetRef.invokeMethodAsync("OnImagePasted", event.target.result);
+                    };
+                    reader.readAsDataURL(blob);
+                }
+            }
+        });
+    }
+};
