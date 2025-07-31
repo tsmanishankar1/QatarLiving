a57fb08fb993c65d96cc6b5b7250c484a1ead852
giftsondanielg <giftsondaniel.g@kryptosinfosys.com>
2025-07-31 10:37:57 +0530

    Allow Only allow PNG or JPEG

--- QLN.ContentBO.WebUI/wwwroot/imagePasteHandler.js
+++ QLN.ContentBO.WebUI/wwwroot/imagePasteHandler.js
@@ -7,13 +7,15 @@
             const items = (event.clipboardData || event.originalEvent.clipboardData).items;
 
             for (const item of items) {
-                if (item.type.indexOf("image") === 0) {
+                // Only allow PNG or JPEG
+                if (item.type === "image/png" || item.type === "image/jpeg") {
                     const blob = item.getAsFile();
                     const reader = new FileReader();
                     reader.onload = function (event) {
                         dotNetRef.invokeMethodAsync("OnImagePasted", event.target.result);
                     };
                     reader.readAsDataURL(blob);
+                    return; // Exit after handling the first image
                 }
             }
         });
