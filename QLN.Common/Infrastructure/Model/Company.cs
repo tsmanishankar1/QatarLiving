1d85568d37bac82a2941c972d64fe5aa2d356911
Kousiki S <kousiki.s@kryptosinfosys.com>
2025-08-05 21:33:51 +0530

    model

---
+++ QLN.Common/Infrastructure/Model/Company.cs
@@ -0,0 +1,70 @@
+﻿using QLN.Common.DTO_s;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLN.Common.Infrastructure.Model
+{
+    public class Company
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid CategoryId { get; set; }
+        [Required]
+        public Guid L1CategoryId { get; set; }
+        [Required]
+        public Guid L2CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+        public string? L1CategoryName { get; set; }
+        public string? L2CategoryName { get; set; }
+        public bool IsPriceOnRequest { get; set; }
+        public decimal? Price { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public string PhoneNumberCountryCode { get; set; } = string.Empty;
+        [Required]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        public string WhatsappNumberCountryCode { get; set; } = string.Empty;
+        [Required]
+        public string WhatsappNumber { get; set; } = string.Empty;
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+        [Required]
+        public string Location { get; set; } = string.Empty;
+        public int? LocationId { get; set; }
+        public string? SubscriptionId { get; set; }
+        [Required]
+        public string ZoneId { get; set; } = string.Empty;
+        public string? StreetNumber { get; set; }
+        public string? BuildingNumber { get; set; }
+        public string? LicenseCertificate { get; set; }
+        public string? Comments { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Lattitude { get; set; }
+        public List<ImageDto>? PhotoUpload { get; set; }
+        public string? UserName { get; set; }
+        public ServiceStatus? Status { get; set; }
+        public bool IsFeatured { get; set; }
+        public bool IsPromoted { get; set; }
+        public bool IsRefreshed => LastRefreshedOn.HasValue && LastRefreshedOn.Value > DateTime.UtcNow;
+        public DateTime? PromotedExpiryDate { get; set; }
+        public DateTime? FeaturedExpiryDate { get; set; }
+        public DateTime? LastRefreshedOn { get; set; }
+        public ServiceAdType AdType { get; set; }
+        public DateTime? PublishedDate { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ QLN.Common/Infrastructure/QLDbContext/QLCompanyContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QLN.Common.Infrastructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLN.Common.Infrastructure.QLDbContext
+{
+    public class QLCompanyContext : DbContext
+    {
+        public QLCompanyContext(DbContextOptions<QLCompanyContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Company> Companies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
