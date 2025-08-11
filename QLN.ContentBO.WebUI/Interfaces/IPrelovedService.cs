7acc87950bee83368324941b0923d71b11ef0e7b
giftsondanielg <giftsondaniel.g@kryptosinfosys.com>
2025-08-11 13:00:53 +0530

    Preloved API Implementation WIP

---
+++ QLN.ContentBO.WebUI/Interfaces/IPrelovedService.cs
@@ -0,0 +1,13 @@
+﻿using QLN.ContentBO.WebUI.Models;
+
+namespace QLN.ContentBO.WebUI.Interfaces
+{
+    public interface IPrelovedService
+    {
+        Task<HttpResponseMessage?> GetPrelovedSubscription(PrelovedSubscriptionQuery request);
+        Task<HttpResponseMessage?> GetPrelovedP2pTransaction(FilterRequest request);
+        Task<HttpResponseMessage?> GetPrelovedUserListing(FilterRequest request);
+        Task<HttpResponseMessage?> GetPrelovedP2pListing(FilterRequest request);
+        Task<HttpResponseMessage?> PerformPrelovedBulkActionAsync(object payload);
+    }
+}
---
+++ QLN.ContentBO.WebUI/Services/PrelovedService.cs
@@ -0,0 +1,60 @@
+﻿using QLN.ContentBO.WebUI.Interfaces;
+using QLN.ContentBO.WebUI.Models;
+using QLN.ContentBO.WebUI.Services.Base;
+
+namespace QLN.ContentBO.WebUI.Services
+{
+    public class PrelovedService : ServiceBase<PrelovedService>, IPrelovedService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger _logger;
+        public PrelovedService(HttpClient httpClient, ILogger<PrelovedService> Logger)
+            : base(httpClient, Logger)
+        {
+            _httpClient = httpClient;
+            _logger = Logger;
+        }
+
+        public Task<HttpResponseMessage?> GetPrelovedP2pListing(FilterRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage?> GetPrelovedP2pTransaction(FilterRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage?> GetPrelovedSubscription(PrelovedSubscriptionQuery query)
+        {
+            var queryParams = new Dictionary<string, string?>
+                {
+                    { "subscriptionType", query.SubscriptionType },
+                    { "filterDate", query.FilterDate },
+                    { "Page", query.Page.ToString() },
+                    { "PageSize", query.PageSize.ToString() },
+                    { "Search", query.Search }
+                };
+
+            var queryString = string.Join("&", queryParams
+                .Where(kvp => !string.IsNullOrWhiteSpace(kvp.Value))
+                .Select(kvp => $"{kvp.Key}={Uri.EscapeDataString(kvp.Value!)}"));
+
+            var requestUrl = $"/api/v2/classifiedbo/preloved-view-subscriptions?{queryString}";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+
+            return await _httpClient.SendAsync(request);
+        }
+
+        public Task<HttpResponseMessage?> GetPrelovedUserListing(FilterRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage?> PerformPrelovedBulkActionAsync(object payload)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
