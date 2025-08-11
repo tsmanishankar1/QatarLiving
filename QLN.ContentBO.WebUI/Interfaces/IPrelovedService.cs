2a13b985af5a0b4ab82adaa4bad3c55cc2a51081
giftsondanielg <giftsondaniel.g@kryptosinfosys.com>
2025-08-11 20:05:07 +0530

    Add P2P service

--- QLN.ContentBO.WebUI/Interfaces/IPrelovedService.cs
+++ QLN.ContentBO.WebUI/Interfaces/IPrelovedService.cs
@@ -4,10 +4,10 @@ namespace QLN.ContentBO.WebUI.Interfaces
 {
     public interface IPrelovedService
     {
-        Task<HttpResponseMessage?> GetPrelovedSubscription(PrelovedSubscriptionQuery request);
-        Task<HttpResponseMessage?> GetPrelovedP2PTransaction(PrelovedP2PTransactionQuery query);
+        Task<HttpResponseMessage?> GetPrelovedSubscription(PrelovedSubscriptionQuery prelovedSubscriptionQuery);
+        Task<HttpResponseMessage?> GetPrelovedP2PTransaction(PrelovedP2PTransactionQuery p2pTransactionQuery);
         Task<HttpResponseMessage?> GetPrelovedUserListing(FilterRequest request);
-        Task<HttpResponseMessage?> GetPrelovedP2pListing(FilterRequest request);
+        Task<HttpResponseMessage?> GetPrelovedP2pListing(PrelovedP2PSubscriptionQuery p2pSubscriptionQuery);
         Task<HttpResponseMessage?> PerformPrelovedBulkActionAsync(object payload);
     }
 }
--- QLN.ContentBO.WebUI/Services/PrelovedService.cs
+++ QLN.ContentBO.WebUI/Services/PrelovedService.cs
@@ -9,6 +9,7 @@ namespace QLN.ContentBO.WebUI.Services
     {
         private readonly HttpClient _httpClient;
         private readonly ILogger _logger;
+
         public PrelovedService(HttpClient httpClient, ILogger<PrelovedService> Logger)
             : base(httpClient, Logger)
         {
@@ -16,9 +17,39 @@ namespace QLN.ContentBO.WebUI.Services
             _logger = Logger;
         }
 
-        public Task<HttpResponseMessage?> GetPrelovedP2pListing(FilterRequest request)
+        public async Task<HttpResponseMessage?> GetPrelovedP2pListing(PrelovedP2PSubscriptionQuery query)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var queryParams = new Dictionary<string, string?>
+                {
+                    { "createdDate", query.CreatedDate },
+                    { "publishedDate", query.PublishedDate },
+                    { "Page", query.Page.ToString() },
+                    { "PageSize", query.PageSize.ToString() },
+                    { "Search", query.Search },
+                    { "SortBy", query.SortBy },
+                    { "SortOrder", query.SortOrder }
+                };
+
+                var queryString = string.Join("&",
+                    queryParams
+                    .Where(kvp => !string.IsNullOrWhiteSpace(kvp.Value))
+                    .Select(kvp => $"{kvp.Key}={Uri.EscapeDataString(kvp.Value!)}"));
+
+                var requestUrl = $"/api/v2/classifiedbo/preloved-p2p-subscriptions?{queryString}";
+
+                var response = await _httpClient.GetAsync(requestUrl);
+
+                var request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+
+                return await _httpClient.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "GetPrelovedP2pListing");
+                return new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+            }
         }
 
         public async Task<HttpResponseMessage?> GetPrelovedP2PTransaction(PrelovedP2PTransactionQuery query)
