b78d053d126ee8f5ce7223c34674688bf827c699
Kousiki S <kousiki.s@kryptosinfosys.com>
2025-08-12 16:02:25 +0530

    Subscription id to guid

--- QLN.Classified.MS/Service/Services/InternalServicesService.cs
+++ QLN.Classified.MS/Service/Services/InternalServicesService.cs
@@ -572,7 +572,7 @@ namespace QLN.Classified.MS.Service.Services
                 BuildingNumber = dto.BuildingNumber,
                 LicenseCertificate = dto.LicenseCertificate,
                 ZoneId = dto.ZoneId,
-                SubscriptionId = dto.SubscriptionId,
+                SubscriptionId = dto.SubscriptionId.ToString(),
                 Comments = dto.Comments,
                 Longitude = (double)dto.Longitude,
                 Lattitude = (double)dto.Lattitude,
