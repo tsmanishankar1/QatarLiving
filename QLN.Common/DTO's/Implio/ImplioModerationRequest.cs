b4f4b47deb3031e9c8a7d33d8c1293ddf23b74d8
Grant Vine <grant.vine@crayon.com>
2025-08-13 13:53:46 +0200

    Implio Moderation integration

---
+++ QLN.Common/DTO's/Implio/ImplioModerationRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace QLN.Common.DTO_s.Implio
+{
+
+    public class ImplioModerationRequest
+    {
+        [JsonPropertyName("id")]
+        public required string Id { get; set; }
+
+        [JsonPropertyName("customerSpecific")]
+        public ImplioCustomerSpecific? CustomerSpecific { get; set; } // not sure what can get populated into this field, so making it nullable
+
+        [JsonPropertyName("content")]
+        public ImplioContent Content { get; set; }
+
+        [JsonPropertyName("user")]
+        public ImplioUser User { get; set; }
+
+        [JsonPropertyName("location")]
+        public ImplioLocation Location { get; set; }
+
+        [JsonPropertyName("result")]
+        public ImplioResult? Result { get; set; }
+    }
+}
---
+++ QLN.Common/DTO's/Implio/ImplioModerationResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace QLN.Common.DTO_s.Implio
+{
+
+    public class ImplioModerationResponse
+    {
+        [JsonPropertyName("batchId")]
+        public required string BatchId { get; set; }
+
+        [JsonPropertyName("accepted")]
+        public List<ImplioAccepted> Accepted { get; set; } = new List<ImplioAccepted>();
+
+        [JsonPropertyName("rejected")]
+        public List<ImplioRejected> Rejected { get; set; } = new List<ImplioRejected>();
+    }
+
+}
