83044b9cb31e33dcc7f696af7ba6540c14130c61
giftsondanielg <giftsondaniel.g@kryptosinfosys.com>
2025-08-14 15:50:33 +0530

    Disable Drag when Landing Item is null #11385

---
+++ QLN.ContentBO.WebUI/Models/IndexedLandingItem.cs
@@ -0,0 +1,10 @@
+﻿using static QLN.ContentBO.WebUI.Models.ClassifiedLanding;
+
+namespace QLN.ContentBO.WebUI.Models
+{
+    public class IndexedLandingItem
+    {
+        public LandingPageItem Item { get; set; }
+        public int Index { get; set; }
+    }
+}
