a68cca422e98c64b608bd46e7ea7b96605ce21f3
Grant Vine <grant.vine@crayon.com>
2025-08-18 13:54:34 +0200

    Minor updates

--- QLN.DataMigration/Models/CsvCategoryMapper.cs
+++ QLN.DataMigration/Models/CsvCategoryMapper.cs
@@ -1,5 +1,6 @@
 
 using CsvHelper.Configuration.Attributes;
+using QLN.Common.DTO_s;
 
 namespace QLN.DataMigration.Models
 {
@@ -8,16 +9,29 @@ namespace QLN.DataMigration.Models
         [Name("ad_id")]
         public string AdId { get; set; } = string.Empty;
 
+        [Ignore]
+        public long? CategoryId { get; set;}
+
         [Name("Main")]
         public string Category { get; set; } = string.Empty;
 
+        [Ignore]
+        public long? L1CategoryId { get; set; }
+
         [Name("L1")]
         public string L1Category { get; set; } = string.Empty;
+
+        [Ignore]
+        public long? L2CategoryId { get; set; }
 
         [Name("L2")]
         public string? L2Category { get; set; }
 
         [Name("snid")]
         public string? SubscriptionId { get; set; } = string.Empty;
+
+        [Ignore]
+        public AdTypeEnum? AdType { get; set; } = AdTypeEnum.Free;
+
     }
 }
