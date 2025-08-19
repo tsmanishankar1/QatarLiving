a6f4c35769c98ee69375e6d2df35bbd52d43ce86
Kishore Kumar Govindaraju <kishorekumar.g@kryptosinfosys.com>
2025-08-19 13:08:57 +0530

    Subscription validation on payments

--- QLN.Common/Infrastructure/QLDbContext/QLApplicationContext.cs
+++ QLN.Common/Infrastructure/QLDbContext/QLApplicationContext.cs
@@ -8,6 +8,8 @@ namespace QLN.Common.Infrastructure.QLDbContext
 {
     public class QLApplicationContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
+        public DbSet<UserSubscription> UserSubscriptions { get; set; }
+        public DbSet<UserCompany> UserCompanies { get; set; }
         public QLApplicationContext(DbContextOptions<QLApplicationContext> options)
             : base(options)
         {
@@ -28,6 +30,26 @@ namespace QLN.Common.Infrastructure.QLDbContext
                     {
                         ownedNavigationBuilder.ToJson();
                     });
+            modelBuilder.Entity<UserSubscription>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.ToTable("UserSubscription");
+
+                entity.HasOne<ApplicationUser>()
+                      .WithMany(u => u.Subscriptions)
+                      .HasForeignKey(s => s.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+            modelBuilder.Entity<UserCompany>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.ToTable("UserCompany");
+
+                entity.HasOne<ApplicationUser>()
+                      .WithMany(u => u.Companies)
+                      .HasForeignKey(s => s.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
         }
     }
 }
