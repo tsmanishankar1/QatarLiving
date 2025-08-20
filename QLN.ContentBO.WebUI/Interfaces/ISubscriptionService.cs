cdd0fe7a58ba911f88844065b0b9e0f5d4a4a5df
giftsondanielg <giftsondaniel.g@kryptosinfosys.com>
2025-08-20 19:06:21 +0530

    Added Subscription Listing API #11902

---
+++ QLN.ContentBO.WebUI/Interfaces/ISubscriptionService.cs
@@ -0,0 +1,9 @@
+﻿namespace QLN.ContentBO.WebUI.Interfaces
+{
+    public interface ISubscriptionService
+    {
+        Task<HttpResponseMessage?> GetAllSubscriptionProductsAsync(int? vertical = null,
+            int? subvertical = null,
+            int? productType = null);
+    }
+}
---
+++ QLN.ContentBO.WebUI/Services/SubscriptionService.cs
@@ -0,0 +1,48 @@
+﻿using QLN.ContentBO.WebUI.Interfaces;
+using QLN.ContentBO.WebUI.Services.Base;
+using System.Net;
+
+namespace QLN.ContentBO.WebUI.Services
+{
+    public class SubscriptionService : ServiceBase<SubscriptionService>, ISubscriptionService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger _logger;
+        public SubscriptionService(HttpClient httpClient, ILogger<SubscriptionService> Logger)
+            : base(httpClient, Logger)
+        {
+            _httpClient = httpClient;
+            _logger = Logger;
+        }
+
+        public async Task<HttpResponseMessage?> GetAllSubscriptionProductsAsync(int? vertical = null,
+            int? subvertical = null,
+            int? productType = null)
+        {
+            try
+            {
+                var queryParams = new List<string>();
+
+                if (vertical.HasValue)
+                    queryParams.Add($"vertical={vertical.Value}");
+                if (subvertical.HasValue)
+                    queryParams.Add($"subvertical={subvertical.Value}");
+                if (productType.HasValue)
+                    queryParams.Add($"productType={productType.Value}");
+
+                var queryString = queryParams.Count > 0 ? "?" + string.Join("&", queryParams) : string.Empty;
+
+                var requestUrl = $"/api/products/getallproducts{queryString}";
+
+                var request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+
+                return await _httpClient.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "GetAllSubscriptionProductsAsync");
+                return new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+            }
+        }
+    }
+}
