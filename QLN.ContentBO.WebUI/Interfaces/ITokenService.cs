a4a02382908ed95513f7ccb5d702f8f43e9e77c9
giftsondanielg <giftsondaniel.g@kryptosinfosys.com>
2025-08-21 20:07:08 +0530

    Auth Init #11943

---
+++ QLN.ContentBO.WebUI/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿namespace QLN.ContentBO.WebUI.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<HttpResponseMessage> UserSync();
+        Task<HttpResponseMessage> GetRefreshToken();
+        Task<HttpResponseMessage> IsValid();
+    }
+}
---
+++ QLN.ContentBO.WebUI/Services/TokenService.cs
@@ -0,0 +1,33 @@
+﻿using QLN.ContentBO.WebUI.Interfaces;
+using QLN.ContentBO.WebUI.Services.Base;
+
+namespace QLN.ContentBO.WebUI.Services
+{
+    public class TokenService : ServiceBase<TokenService>, ITokenService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<TokenService> _logger;
+
+        public TokenService(HttpClient httpClient, ILogger<TokenService> logger)
+            : base(httpClient, logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+
+        public Task<HttpResponseMessage> GetRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> IsValid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> UserSync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
