8271fca90eec0071cb73612be39dabaddca93484
giftsondanielg <giftsondaniel.g@kryptosinfosys.com>
2025-08-22 00:51:37 +0530

    GetBOAPIBaseUrl

--- QLN.ContentBO.WebUI/Program.cs
+++ QLN.ContentBO.WebUI/Program.cs
@@ -14,7 +14,7 @@ logger.Debug("init main");
 try
 {
     var builder = WebApplication.CreateBuilder(args);
-    var BOAPIBaseUrl = builder.Configuration["ServiceUrlPaths:ContentBOAPI"];
+    var BOAPIBaseUrl = builder.Configuration["ServiceUrlPaths:BOAPIBaseUrl"];
     if (string.IsNullOrWhiteSpace(BOAPIBaseUrl))
     {
         throw new InvalidOperationException("Content Back Office API URL is missing in the configuration file.");
